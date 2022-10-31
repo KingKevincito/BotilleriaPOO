ecf1976fa7d1dbf42b9cef69f673b6982a579eaa
KingKevincito <116766673+KingKevincito@users.noreply.github.com>
2022-10-31 08:53:55 -0300

    Update Botilleria.py

--- Botilleria.py
+++ Botilleria.py
@@ -3,12 +3,12 @@ class Drinkable
     ##Atributos##
     
     name=""               #nombre del bebestible
-    code=""               #codigo de barra del bebestible
-    sale_price=""         #valor del bebestible
-    expiration_date=""    #fecha de expiracion del bebestible
-    ML=""                 #la cantidad de Mililitros que contiene el embase
-    stock=""              #cantidad del bebestible elegido en la tienda 
-    sale_restriction=""   #la edad necesaria para la compra del bebestible
+    code=0                #codigo de barra del bebestible
+    sale_price=0          #valor del bebestible
+    expiration_date=0     #fecha de expiracion del bebestible
+    ML=0                  #la cantidad de Mililitros que contiene el embase
+    stock=0               #stock del bebestible en la tienda
+    sale_restriction=0    #la edad necesaria para la compra del bebestible
     type=""               #si es alcoholica y/o fantasia 
     brand=""              #marca del bebestible
     material=""           #material del embase
