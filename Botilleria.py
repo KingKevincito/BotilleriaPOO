8e125ff39d7bde4d5d28d85a74923638ec0037e9
KingKevincito <116766673+KingKevincito@users.noreply.github.com>
2022-10-31 08:42:04 -0300

    Create Botilleria.py
    Tarea de POO

---
+++ Botilleria.py
@@ -0,0 +1,29 @@
+class Drinkable
+{
+    ##Atributos##
+    
+    name=""               #nombre del bebestible
+    code=""               #codigo de barra del bebestible
+    sale_price=""         #valor del bebestible
+    expiration_date=""    #fecha de expiracion del bebestible
+    ML=""                 #la cantidad de Mililitros que contiene el embase
+    stock=""              #cantidad del bebestible elegido en la tienda 
+    sale_restriction=""   #la edad necesaria para la compra del bebestible
+    type=""               #si es alcoholica y/o fantasia 
+    brand=""              #marca del bebestible
+    material=""           #material del embase
+    seals=                #manda un True o False segun si contiene sellos o no 
+    
+    
+    ##Fin seccion de Atributos##
+    
+    ##Metodos##
+    
+    update_stock()            #actualizacion stock del bebestible que hay en la botilleria
+    order_expiration_date()   #ordenar en estantes segun fecha de vencimiento
+    total_sales()             #las ventas totales de un dia 
+    selling_price_update()    #actualizacion precio del bebestible
+    payment_method()          #el metodo por el cual se pago el bebestible
+    
+    ##Fin seccion de Metodos##
+}
